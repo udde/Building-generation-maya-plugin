8b138858ba828681e012439c8eebf36467a65b6e
Syllvan <johansylvan1@gmail.com>
2015-12-07 10:43:07 +0100

    Added ability to generate buildings of arbitrary dimensions

--- building.py
+++ building.py
@@ -2,18 +2,22 @@ import sys
 import maya.OpenMaya as OpenMaya
 import maya.OpenMayaMPx as OpenMayaMPx
 import maya.cmds as cmds
+import random
 
 kPluginCmdName = "build"
+
 class housePart():
-    def __init__(self, dimension, rof_spec, position):
-        self.pos    = [0, 0, 0]
-        self.dim    = [1, 1, 1]
+    def __init__(self, dimension, roofSpec, position):
+        self.pos = [0, 0, 0]
+        self.dim = dimension
+        self.roof = roofSpec
+
     def build(self):
         i = 0
-        cmds.polyCube(n='box'+str(i))
-        cmds.move(0,0,0,r=1)
-        cmds.polyCube(n='roof'+str(i))
-        cmds.move(0,1,0,r=1)
+        cmds.polyCube(sx=5, sy=5, sz=5, width=self.dim[0], height=self.dim[1], depth=self.dim[2], n='box'+str(i))
+        cmds.move(0,self.dim[1]/2,0,r=1)
+        cmds.polyCube(width=self.dim[0], height=self.roof[1], depth=self.dim[2], n='roof'+str(i))
+        cmds.move(0,self.dim[1] + self.roof[1]/2,0,r=1)
         cmds.select('roof'+str(i)+'.vtx[2:3]')
         cmds.move(0,-0.5,-0.5,r=1)
         cmds.select('roof'+str(i)+'.vtx[4:5]')
@@ -25,12 +29,22 @@ class house():
         self.lot_pos = lot_pos
         #store the different house parts here
         self.parts = []
+        self.houseHeight = random.randint(3,8)
+        self.houseWidth = random.randint(3,8)
+        self.houseDepth = random.randint(3,8)
+        self.roofHeight = random.randint(3,4)
+        self.roofType = random.randint(1,3)
+        self.lawnX = 15
+        self.lawnZ = 15
 
     def generateMainPart(self):
         #build the main part of the house
 
         #after some calculations
-        self.parts.append( housePart([1,1,1], 0.5, [0,0,0]) )
+        self.parts.append( housePart([self.houseWidth, self.houseDepth, self.houseHeight], [self.roofType, self.roofHeight], [0,0,0]) )
+
+    #def lotPlacement(self):
+     #   cmds.move( random.randint(-(self.lawnX)/2+self.width,self.lawnX/2-self.width), self.height/2 +0.2, random.randint(-(self.lawnZ)/2+self.depth,self.lawnZ/2-self.depth), 'cube1', absolute=True )
 
     def extend(self):
         #extend the house if its posible
